340ea3d43d552f809c6d1be565e5decefef13b63
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-09-27 23:38:35 +0530

    TravelBus Architecture

---
+++ app/src/main/java/com/example/travelbus/views/adapter/TravelBusApp.kt
@@ -0,0 +1,8 @@
+package com.example.travelbus.views.adapter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TravelBusApp:Application() {
+}
---
+++ app/src/main/java/com/example/travelbus/views/adapter/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelbus.views.adapter.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travelbus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
