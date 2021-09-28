eb14abd51ebeebcf91f6f10f29b4f495d18c48dc
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-09-28 14:55:38 +0530

    Added Firebase

---
+++ app/src/main/java/com/example/travelbus/views/adapter/activities/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.travelbus.views.adapter.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travelbus.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+}
