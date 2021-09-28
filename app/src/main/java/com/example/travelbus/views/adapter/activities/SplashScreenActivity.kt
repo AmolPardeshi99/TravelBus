e6903f7ba441d5f2fecb907053c50e251a3a173d
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-09-28 21:41:55 +0530

    SignIn And OnBoarding Fragment

--- app/src/main/java/com/example/travelbus/views/adapter/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/travelbus/views/adapter/activities/SplashScreenActivity.kt
@@ -1,13 +1,29 @@
 package com.example.travelbus.views.adapter.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import com.example.travelbus.R
+import com.example.travelbus.views.adapter.fragments.Country_LanguageFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.anko.startActivity
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            startActivity<OnBoardingActivity>()
+            finish()
+        }
     }
 
 }
--- app/src/main/java/com/example/travelbus/views/adapter/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/travelbus/views/adapter/fragments/LoginFragment.kt
@@ -1,12 +1,12 @@
 package com.example.travelbus.views.adapter.fragments
 
 import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.travelbus.R
 
-class Country_LanguageFragment : Fragment(R.layout.fragment_country__language) {
-
-
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
 }
