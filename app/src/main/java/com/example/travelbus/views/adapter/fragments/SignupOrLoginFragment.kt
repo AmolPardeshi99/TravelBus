95b7cd42d1a1eb8920147a9511ac142b215be4ec
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-09-29 13:18:57 +0530

    Login/Signup Fragment added.

---
+++ app/src/main/java/com/example/travelbus/views/adapter/fragments/SignupOrLoginFragment.kt
@@ -0,0 +1,13 @@
+package com.example.travelbus.views.adapter.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.travelbus.R
+
+
+class SignupOrLoginFragment :  Fragment(R.layout.fragment_signup_or_login) {
+
+}
