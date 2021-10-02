87b1a5fe240c5632b41d481ed48d21bd6d94ce56
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-10-02 15:45:59 +0530

    SelectJourneyTypeFragment.kt added.

---
+++ app/src/main/java/com/example/travelbus/views/adapter/fragments/bus_hire/SelectJourneyTypeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.travelbus.views.adapter.fragments.bus_hire
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.travelbus.R
+
+class SelectJourneyTypeFragment : Fragment(R.layout.fragment_select_journey_type) {
+
+}
