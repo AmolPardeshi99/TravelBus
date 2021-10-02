c79071131b601c204e7c6c12a4d002813e344b65
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-10-02 19:41:18 +0530

    added bus hire and resolved a error in slpash screen

---
+++ app/src/main/java/com/example/travelbus/views/adapter/fragments/FillContactDetailFragment.kt
@@ -0,0 +1,8 @@
+package com.example.travelbus.views.adapter.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.travelbus.R
+
+class FillContactDetailFragment : Fragment(R.layout.fragment_fill_contact_detail) {
+
+}
